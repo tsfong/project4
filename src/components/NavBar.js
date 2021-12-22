028b4cce204a64ec1cd46394035550c41d8a8faa
tsfong <tsfong@uci.edu>
2021-12-22 15:27:49 -0800

    adjusting navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,8 +12,8 @@ const NavBar = () => (
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
             <Link className="nav-link" to="/about">About</Link>
-            <Link className="nav-link" to="/create">Create</Link>
             <Link className="nav-link" to="/gallery">Gallery</Link>
+            <Link className="nav-link" to="/create">Create</Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
